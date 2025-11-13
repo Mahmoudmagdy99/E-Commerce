c15212e1aa5a86d5221b08ef590faa6c1be3bc9c
Mahmoud <mahmoudkotamy97@gmail.com>
2025-11-13 21:49:00 +0200

    backend-apply error handling

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +17,8 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
+builder.Services.AddCors();
+
 
 var app = builder.Build();
 
@@ -24,6 +27,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
