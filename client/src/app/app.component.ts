4942e965ba914f70c6b029c427ff4287f5dc8653
Mahmoud <mahmoudkotamy97@gmail.com>
2025-11-16 18:42:46 +0200

    frontend-initial design and connecting with backend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
   //imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [HeaderComponent],
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
+  
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
   title = 'client';
+  products: Product[]= [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log('Request completed');
+      }
+    });
+  }
+
 }
