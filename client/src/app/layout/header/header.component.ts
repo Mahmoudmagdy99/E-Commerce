4e9d95fc30a01c1b9f0743ab6a8714226dc10ad5
Mahmoud <mahmoudkotamy97@gmail.com>
2025-11-18 15:42:00 +0200

    forntend-feat: implement shop feature with product listing, filtering, and sorting

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
-import {MatIcon} from '@angular/material/icon';
-import {MatButton} from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
 import { MatBadge } from '@angular/material/badge';
 
 @Component({
   selector: 'app-header',
-  imports: [
-    MatIcon,
-    MatButton,
-    MatBadge
-  ],
+  imports: [MatIcon, MatButton, MatBadge],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  isOpen = false;
 
+  toggleMenu(): void {
+    this.isOpen = !this.isOpen;
+  }
 }
