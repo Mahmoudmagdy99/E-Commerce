e7f6738543c9b2ced80ad2cd63bac7ef1f7a1845
Mahmoud <mahmoudkotamy97@gmail.com>
2025-12-12 00:54:03 +0200

    FINAL: add initial Setup class with commented commands for development

---
+++ API/Setup.cs
@@ -0,0 +1,12 @@
+
+namespace API;
+
+public class Setup
+{
+    //dotnet ef database update -p Infrastructure -s API
+    //docker compose down
+    //docker compose up -d
+    //meet-elephant-7933.upstash.io:6379,Password=AR79AAImcDFlZWFiYjJmOGI4MjQ0YTdlYTgzYTY2NTg0YWU3N2I5MXAxNzkzMw,ssl=true,abortConnection=false
+    //stripe listen --forward-to https://localhost:5001/api/payments/webhook -e payment_intent.succeeded
+    //stripe trigger payment_intent.succeeded
+}
